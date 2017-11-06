7c8fdf7bd25b9d96fc60a3128960c7b3115f035d
Glen Smith <glen@bytecode.com.au>
2017-11-06 16:44:07 +1100

    Learning more about pagination

--- src/app/quote-table/quote-table.component.ts
+++ src/app/quote-table/quote-table.component.ts
@@ -3,7 +3,14 @@ import {Quote} from '../quote';
 import {QuoteService} from '../quote.service';
 import {MatPaginator, MatSnackBar} from '@angular/material';
 import {DataSource} from '@angular/cdk/collections';
+
 import {Observable} from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/combineLatest';
+
 
 @Component({
   selector: 'app-quote-table',
@@ -12,19 +19,24 @@ import {Observable} from 'rxjs/Observable';
 })
 export class QuoteTableComponent implements OnInit {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
   quotesDataSource: QuoteDataSource;
+
+  dbSize = 10;
 
   displayedColumns = ['quoteAuthor', 'quoteText'];
 
   constructor(private quoteService: QuoteService,
               private snackBar: MatSnackBar) {
 
+
   }
 
   ngOnInit() {
 
-    //this.snackBar.open("Let's load some quotes..", "Opening", {duration:2000});
-    this.quotesDataSource = new QuoteDataSource(this.quoteService);
+    this.quotesDataSource = new QuoteDataSource(this.quoteService, this.paginator);
+
   }
 
 
@@ -32,13 +44,27 @@ export class QuoteTableComponent implements OnInit {
 
 class QuoteDataSource extends DataSource<any> {
 
-  constructor(private quoteService: QuoteService) {
+  constructor(private quoteService: QuoteService, private paginator: MatPaginator) {
     super();
   }
 
   connect(): Observable<Quote[]> {
-    console.log("Datasource is loading..");
-    return this.quoteService.getQuotes();
+    console.log('Datasource is loading..');
+
+
+    return Observable.combineLatest(this.quoteService.getQuotes(), this.paginator.page).map((value) => {
+      console.log('Something emitted', value);
+      let quotesDb = value[0];
+      let pageInfo = value[1];
+
+      this.paginator.length = quotesDb.length;
+
+      // Grab the page's slice of data.
+      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return quotesDb.splice(startIndex, this.paginator.pageSize);
+    });
+
+
   }
 
   disconnect() {
